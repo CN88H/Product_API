d3f92053a43b5dfa7b3b3256187dbb84f80d9c89
CN88H <cheenou88@gmail.com>
2022-06-13 00:54:22 -0500

    Changed database setting + Created Product App

---
+++ products_project/local_setting.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-u$rr9w*ak+1viizv52r*)%nb%8vl4(7cq2trqvh!#oh8659+oq'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'products_database',
+        'Host': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Damnbaka.88'
+    }
+}
