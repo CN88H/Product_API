dd4f2e67a7a09a8b88f7c258e639738933b1bdde
CN88H <cheenou88@gmail.com>
2022-06-13 04:27:04 -0500

    Created New View funciton for Getting Product by id + Added New URLs Path

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,10 +9,10 @@ from .models import Product
 def products_list(request):
 
     if request.method == 'GET':
-
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
 
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
@@ -19,3 +20,10 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
